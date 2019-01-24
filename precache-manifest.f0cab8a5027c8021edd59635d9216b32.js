350136cc17d6f8e9c4595b9f8b5eaa2e18e03fbf
HalimMahmoud <halim_mahmoud50@yahoo.com>
2019-01-24 18:19:44 +0200

    first commit

---
+++ precache-manifest.f0cab8a5027c8021edd59635d9216b32.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "7426bc596893ad8da2cb",
+    "url": "/course/static/js/6.7426bc59.chunk.js"
+  },
+  {
+    "revision": "f579809fa284865d533d",
+    "url": "/course/static/js/0.f579809f.chunk.js"
+  },
+  {
+    "revision": "c6413e9404590dd326bc",
+    "url": "/course/static/js/main.c6413e94.chunk.js"
+  },
+  {
+    "revision": "3e3c7a364f844ea3a847",
+    "url": "/course/static/js/runtime~main.3e3c7a36.js"
+  },
+  {
+    "revision": "ab6386ae06aff6f80d5c",
+    "url": "/course/static/js/2.ab6386ae.chunk.js"
+  },
+  {
+    "revision": "e7f97db1228152d96594",
+    "url": "/course/static/js/3.e7f97db1.chunk.js"
+  },
+  {
+    "revision": "0257a1844b17bbe5562d",
+    "url": "/course/static/js/4.0257a184.chunk.js"
+  },
+  {
+    "revision": "62f965619de5f70bf585",
+    "url": "/course/static/js/5.62f96561.chunk.js"
+  },
+  {
+    "revision": "2ced60fb787fb7e8e2bf",
+    "url": "/course/static/js/11.2ced60fb.chunk.js"
+  },
+  {
+    "revision": "24ae3721131db752ef64",
+    "url": "/course/static/js/7.24ae3721.chunk.js"
+  },
+  {
+    "revision": "4975e736d8aa531ecb72",
+    "url": "/course/static/js/8.4975e736.chunk.js"
+  },
+  {
+    "revision": "029645cc199629625ee3",
+    "url": "/course/static/js/9.029645cc.chunk.js"
+  },
+  {
+    "revision": "5643b37b6bde70373d63",
+    "url": "/course/static/js/10.5643b37b.chunk.js"
+  },
+  {
+    "revision": "c6413e9404590dd326bc",
+    "url": "/course/static/css/main.ca15a891.chunk.css"
+  },
+  {
+    "revision": "ab6386ae06aff6f80d5c",
+    "url": "/course/static/css/2.0de34267.chunk.css"
+  },
+  {
+    "revision": "b85832f10668ab4c8dcea0c2a9a39a6f",
+    "url": "/course/index.html"
+  }
+];
